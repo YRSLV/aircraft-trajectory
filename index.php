6dec16871b188119db23c28127694201219bd343
Alex Kharchenko <doom9936.1999@gmail.com>
2018-11-27 00:45:11 +0200

    re-align 8ht motionpath

--- index.php
+++ index.php
@@ -720,7 +720,7 @@ ini_set('error_reporting', E_ALL);
         var chart_li = this.getChartLayoutInterface();
         var chart_area = chart_li.getChartAreaBoundingBox();
         document.querySelector('#chart_div_mp8').style.top = Math.floor(chart_li.getYLocation(dataTable.getValue(0, 0))) - 302 + "px";
-        document.querySelector('#chart_div_mp8').style.left = Math.floor(chart_li.getXLocation(dataTable.getValue(0, 0))) - 160 + "px";
+        document.querySelector('#chart_div_mp8').style.left = Math.floor(chart_li.getXLocation(dataTable.getValue(0, 0))) - 157 + "px";
       };
       google.visualization.events.addListener(chart, 'ready', placeMarker.bind(chart, chart_data));
       chart.draw(chart_data, chart_options);
